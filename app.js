5bdbc33ef7860065e7dcadd162ebb5cd78b7f403
Morgado <morgadodesarrollador@gmail.com>
2021-12-17 00:33:16 +0100

    app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hola Docker\n');
+  res.send('Hola Campeon, has triunfado !!\n');
 });
 
 app.listen(PORT, HOST);
