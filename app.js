904bd1f41777e85cc2a800fe0d26866fdb1c617c
Morgado <morgadodesarrollador@gmail.com>
2021-12-17 10:35:11 +0100

    app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   console.log('hola caracola');
-  res.send('<h1> bienvenidos !!\n');
+  res.send('<h1> bienvenidos al concierto !!\n');
 });
 
 app.listen(PORT, HOST);
