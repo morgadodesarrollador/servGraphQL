e59e67bbb857f116ca0d00d5197352b76dc53e27
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2021-12-18 20:40:25 +0100

    app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   console.log('hola caracola');
-  res.send('<h1> Viva Jenkins hasta que  funcionar !!</h1>\n');
+  res.send('<h1> Viva Jenkins sdsdshasta que  funcionar !!</h1>\n');
 });
 
 app.listen(PORT, HOST);
